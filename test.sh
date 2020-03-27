03065a2fd7c081f09b4b592e1a3de67db3d8a4d6
damoder3536 <62740846+damoder3536@users.noreply.github.com>
2020-03-27 20:24:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ssh 13.234.115.103 mkdir /root/jenkins/
-ssh 13.234.115.103 cd /root/jenkins/ 
-ssh 13.234.115.103 touch jenkins.py
+mkdir /root/jenkins/
+cd /root/jenkins/ 
+touch jenkins.py
